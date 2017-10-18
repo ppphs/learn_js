749295273b7bbbf6c928c0188d3d194ed091c938
phs <jo273175093@qq.com>
2017-10-18 10:17:25 +0800

    添加注释

--- 正则表达式/为金额加逗号.js
+++ 正则表达式/为金额加逗号.js
@@ -1,5 +1,6 @@
 var money = 120300000.11
 let addComma = (money) => {
+  // replace第一个参数正则后面带g的话就会重复执行回调全局字符串操作
   return money.toString().replace(/\d(?=(\d{3})+\.)/g, function (str) { // "(?=exp)" 匹配exp前面的位置 断言
     return str + ','
   })
