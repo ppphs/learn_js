7c50ba5f3c2b49425aa50e0ee24f742e3e0ed38d
phs <jo273175093@qq.com>
2017-10-22 11:39:57 +0800

    +1

---
+++ ES6/String.js
@@ -0,0 +1,17 @@
+// includes
+console.log('abc'.includes(b)) // true
+
+// startsWith
+console.log('abc'.startsWith('a')) // true
+
+// endsWith
+console.log('abc'.endsWith('c')) // true
+
+// repeat
+console.log('abc'.repeat(3)) // 'abcabcabc'
+
+// padStart 从开头填充直到达到长度
+console.log('abc'.padStart(10, '0')) // '0000000abc'
+
+// padEnd
+console.log('abc'.padEnd(10, '0')) // 'abc0000000'
