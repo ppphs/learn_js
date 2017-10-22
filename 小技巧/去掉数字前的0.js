4dcb65b9821dd3bdef61e191cf714239a345b642
phs <jo273175093@qq.com>
2017-10-22 17:38:49 +0800

    +1

---
+++ 小技巧/去掉数字前的0.js
@@ -0,0 +1,7 @@
+// 正则
+var num = '000100'
+console.log(num.replace(/\b(0)+/g, ''))
+
+// 交给js引擎数字化
+var num = '000100'
+console.log(+num)
---
+++ 小技巧/四舍五入保留两位小数.js
@@ -0,0 +1,4 @@
+// toFixed
+var num = 3.1415926
+console.log(num.toFixed(2)) // 3.14
+console.log(num.toFixed(4)) // 3.1416
